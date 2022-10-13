29cfeea39597e15ab4ff1c890d6af73ecfb176ca
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-10-13 12:48:42 +0200

    esercizio 15

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "express-async-errors";
+import axios from "axios";
 
 const app = express();
 
@@ -7,5 +8,13 @@ const app = express();
 app.get("/username", (request, response) => {
     response.json([{ nome: "Riccardo", cognome: "Mannino" }]);
 });
+
+// app.get("/math", (request, response) => {
+//     axios.get("http://numbersapi.com/random/math").then((res) => {
+//         response.send(res.data);
+//     });
+
+//     response.header("Access-Control-Allow-Origin", "*");
+// });
 
 export default app;
